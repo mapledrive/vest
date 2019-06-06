2e1aade6c889de007268988fb5040c45b6045e7b
Sait <32491972+mapledrive@users.noreply.github.com>
2019-06-06 12:07:24 +0500

    Update main.js

--- client/src/actions/main.js
+++ client/src/actions/main.js
@@ -9,7 +9,7 @@ export const getRepos = username => async dispatch => {
 	if (username === "2") {rangestart = '2017-01-01'};
 	if (username === "5") {rangestart = '2014-01-01'};
 	if (username === "10") {rangestart = '2009-01-01'};
-	var url = `http://ec2-54-84-182-191.compute-1.amazonaws.com:8080/vest/v1/charts?code=gc&rs=${rangestart}&re=${rangeend}`;
+	var url = `https://api.thevest.org/vest/v1/charts?code=gc&rs=${rangestart}&re=${rangeend}`;
     const response = await fetch(url);
     const responseBody = await response.json();
     dispatch(addRepos(responseBody));
@@ -29,7 +29,7 @@ export const getStats = dates => async dispatch => {
 	if (dates === "2") {rangestart = '2017-01-01'};
 	if (dates === "5") {rangestart = '2014-01-01'};
 	if (dates === "10") {rangestart = '2009-01-01'};
-	var url = `http://ec2-54-84-182-191.compute-1.amazonaws.com:8080/vest/v1/stats?code=gc&rangeStart=${rangestart}&rangeEnd=${rangeend}&periodStart=2017-01-01&periodEnd=2018-03-01`;
+	var url = `https://api.thevest.org/vest/v1/stats?code=gc&rangeStart=${rangestart}&rangeEnd=${rangeend}&periodStart=2017-01-01&periodEnd=2018-03-01`;
 	const response = await fetch(url);
     const responseBody = await response.json();
     dispatch(addStats(responseBody));

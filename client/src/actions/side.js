8a18b1b787adf81feb3a61d3bba180ae2c21d738
Sait <32491972+mapledrive@users.noreply.github.com>
2019-06-06 12:09:08 +0500

    new endpoint

--- client/src/actions/side.js
+++ client/src/actions/side.js
@@ -5,7 +5,7 @@ export const getRepos = dates => async dispatch => {
   try {
 	var rangestart = dates.startDate.toISOString().substring(0, 10);
 	var rangeend = dates.endDate.toISOString().substring(0, 10);
-	var url = `http://ec2-54-84-182-191.compute-1.amazonaws.com:8080/vest/v1/charts?code=gc&rs=${rangestart}&re=${rangeend}`;
+	var url = `https://api.thevest.org/vest/v1/charts?code=gc&rs=${rangestart}&re=${rangeend}`;
     const response = await fetch(url);
     const responseBody = await response.json();
     dispatch(addRepos(responseBody));
@@ -20,7 +20,7 @@ export const getStats = dates => async dispatch => {
   try {
 	var rangestart = dates.startDate.toISOString().substring(0, 10);
 	var rangeend = dates.endDate.toISOString().substring(0, 10);
-	var url = `http://ec2-54-84-182-191.compute-1.amazonaws.com:8080/vest/v1/stats?code=gc&rangeStart=${rangestart}&rangeEnd=${rangeend}&periodStart=2017-01-01&periodEnd=2018-03-01`;
+	var url = `https://api.thevest.org/vest/v1/stats?code=gc&rangeStart=${rangestart}&rangeEnd=${rangeend}&periodStart=2017-01-01&periodEnd=2018-03-01`;
 	const response = await fetch(url);
     const responseBody = await response.json();
     dispatch(addStats(responseBody));
